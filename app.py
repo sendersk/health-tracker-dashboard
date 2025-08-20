3cd7e15f449e84ef004e4310d52ce47dcb9d2ae5
psend <psenderski94@gmail.com>
2025-08-20 09:23:17 +0200

    Initial commit, create basic structure, add jinjas

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# define homepage route and index() view function
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    if request.method == 'POST':
+        # process form data here
+        return redirect(url_for('dashboard'))
+    return render_template('form.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# run the app in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
